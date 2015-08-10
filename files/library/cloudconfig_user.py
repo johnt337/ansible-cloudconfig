12b2ad10595bab7647024fd9c62e588a3bf17d86
johnt337 <enfermo337@yahoo.com>
2015-08-10 00:50:41 -0400

    test

--- files/library/cloudconfig_user.py
+++ files/library/cloudconfig_user.py
@@ -370,8 +370,8 @@ def main():
             elif rc == 0 and re.match("updating user",out) is not None:
                 result['changed'] = True
 
-        # if rc is not None and rc != 0:
-        #     module.fail_json(name=user.name, msg=err, rc=rc)
+        if rc is not None and rc != 0:
+            module.fail_json(name=user.name, msg=err, rc=rc)
 
         # obscure some stuff from the log
         if user.password is not None:
@@ -379,10 +379,8 @@ def main():
         if user.ssh_authorized_keys is not None:
             result['ssh_authorized_keys'] = '**********'
 
-    # if rc is None:
-    #     result['changed'] = False
-    # if rc == 0:
-    #     result['changed'] = True
+    if rc is None:
+        result['changed'] = False
     
     if rc > 0:
         result['changed'] = False
