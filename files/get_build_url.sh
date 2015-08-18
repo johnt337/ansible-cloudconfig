919185caf8f6b5aeefd24d04d7f6a11285ccc489
John Torres <john.torres@routetodigital.com>
2015-08-18 17:27:46 -0400

    one more time

--- files/get_build_url.sh
+++ files/get_build_url.sh
@@ -15,7 +15,7 @@ project=$2
 ver=`curl -s -X GET https://circleci.com/api/v1/project/${author}/${project} | grep build_num | head -1 | awk -F: '{print\$2}' | sed -e 's/[ ,]//g'`
 #echo "latest build number - ${ver}"
 #echo "getting download url for build number - ${ver}"
-url=`curl -s -X GET https://circleci.com/api/v1/project/${author}/${project}/${ver}/artifacts | grep bin | head -1 | grep url | awk -F: '{print\$2":"\$3}' | sed -e 's/[ ",]//g'`
+url=`curl -s -X GET https://circleci.com/api/v1/project/${author}/${project}/${ver}/artifacts | grep bin | grep url | awk -F: '{print\$2":"\$3}' | sed -e 's/[ ",]//g'`
 #echo "downloading binary for build number - ${ver}"
 echo "${url}"
 #curl -s -L -X GET 'https://${url}' -o '${destination}'
